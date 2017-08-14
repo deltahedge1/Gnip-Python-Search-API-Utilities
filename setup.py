ef2e27a39e9f97d9821a36561591895a9baa7ac4
fionapigott <fpigott@twitter.com>
2017-08-14 14:19:43 -0600

    edit version number

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@ from distutils.core import setup
 
 setup(
     name='gapi',
-    version='1.0.2',
+    version='1.1.0',
     author='Scott Hendrickson, Josh Montague, Jeff Kolb, Fiona Pigott',
-    author_email='scott@drskippy.net',
+    author_email='des-services@twitter.com',
     packages=['search'],
     scripts=['gnip_search.py', 'gnip_time_series.py'],
-    url='https://github.com/DrSkippy/Gnip-Python-Search-API-Utilities',
-    download_url='https://github.com/DrSkippy/Gnip-Python-Search-API-Utilities/tags/',
+    url='https://github.com/fionapigott/Gnip-Python-Search-API-Utilities',
+    download_url='https://github.com/fionapigott/Gnip-Python-Search-API-Utilities/tags/',
     license='LICENSE.txt',
     description='Simple utilties to explore the Gnip search API',
     install_requires=[
-         "tweet_parser >=1.0.3",
+         "tweet_parser >=1.0.5",
          "sngrams >= 0.2.0" ,
          "requests > 2.4.0"
         ],
